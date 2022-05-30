617af9fd1122cac799aee64ebe35bb0f069f89fb
Danila Fomin <dangdancheg@gmail.com>
2022-05-30 20:31:04 +0300

    union types in functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log("test");
-console.log("stuff");
+let greet;
+let great = () => {
+    console.log("alloha buddy");
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(1, 123, 12312);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,12 @@
-console.log("test");
+let greet: Function;
 
-console.log("stuff");
+let great = () => {
+  console.log("alloha buddy");
+};
+
+const add = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(1, 123, 12312);
