5a48b2358321b8058c42d95a70b9646a8a030cd9
Danila Fomin <dangdancheg@gmail.com>
2022-05-30 20:31:27 +0300

    optional parameters in funcitons end

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,10 +1,6 @@
 "use strict";
 let greet;
-let great = () => {
-    console.log("alloha buddy");
-};
 const add = (a, b, c = 10) => {
     console.log(a + b);
     console.log(c);
 };
-add(1, 123, 12312);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,12 +1,6 @@
 let greet: Function;
 
-let great = () => {
-  console.log("alloha buddy");
-};
-
 const add = (a: number, b: number, c: number | string = 10) => {
   console.log(a + b);
   console.log(c);
 };
-
-add(1, 123, 12312);
