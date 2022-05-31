71d91736ff9ac7df313de85abd96f0e2ecf44f1e
Danila Fomin <dangdancheg@gmail.com>
2022-05-31 07:29:18 +0300

    setted access to input fields

--- public/app.js
+++ public/app.js
@@ -1,2 +1,6 @@
 "use strict";
 const form = document.querySelector(".new-item-form");
+const type = document.querySelector("#type");
+const toForm = document.querySelector("#toForm");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,5 @@
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toForm = document.querySelector("#toForm") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
