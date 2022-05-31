7ee885eaf79fe130162fa95bf56f54682b74f768
Danila Fomin <dangdancheg@gmail.com>
2022-05-31 07:33:03 +0300

    added interaction with web forms

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 const type = document.querySelector("#type") as HTMLSelectElement;
-const toForm = document.querySelector("#toForm") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log(type, tofrom, details, amount);
+});
