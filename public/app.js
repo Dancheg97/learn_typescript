5514b549cda1c37cec6c1638cda7caafe18595fa
DANCHE <dangdancheg@gmail.com>
2022-06-05 11:31:04 +0300

    added interface storage for all interfaces

--- public/app.js
+++ public/app.js
@@ -4,13 +4,14 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+let stuff = [];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    if (type.value === "Payment") {
+        stuff.push(new Invoice(tofrom.value, details.value, amount.valueAsNumber));
+    }
 });
-const i1 = new Invoice("mario", "work on my website", 250);
-const i2 = new Invoice("luigi", "work in my car", 120);
-let invoices = [i1, i2];
-invoices.forEach((e) => {
-    console.log(e.amount, e.client, e.format());
+stuff.forEach((e) => {
+    console.log("ss", e.format());
 });
--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,16 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+let stuff: HasFormatter[] = [];
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  if (type.value === "Payment") {
+    stuff.push(new Invoice(tofrom.value, details.value, amount.valueAsNumber));
+  }
 });
 
-const i1 = new Invoice("mario", "work on my website", 250);
-const i2 = new Invoice("luigi", "work in my car", 120);
-
-let invoices: Invoice[] = [i1, i2];
-
-invoices.forEach((e) => {
-  console.log(e.amount, e.client, e.format());
+stuff.forEach((e) => {
+  console.log("ss", e.format());
 });
