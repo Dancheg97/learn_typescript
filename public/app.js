823ec4f0f24998927d5193548812bce17c5fb195
Danila Fomin <dangdancheg@gmail.com>
2022-06-07 20:09:03 +0300

    added ability to add elements

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+import { ListTemplate } from "./classes/has_tmplt.js";
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/payments.js";
 const form = document.querySelector(".new-item-form");
@@ -5,18 +6,16 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
-let stuff = [];
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
     if (type.value === "payment") {
-        stuff.push(new Invoice(tofrom.value, details.value, amount.valueAsNumber));
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
     else {
-        stuff.push(new Payment(tofrom.value, details.value, amount.valueAsNumber));
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(stuff);
-});
-stuff.forEach((e) => {
-    console.log("ss", e.format());
+    list.render(doc, "Invoice", "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { ListTemplate } from "./classes/has_tmplt.js";
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/payments.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
@@ -8,22 +9,16 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
-const template = document.querySelector("ul");
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  let doc: HasFormatter;
   if (type.value === "payment") {
-    var invoice = new Invoice(
-      tofrom.value,
-      details.value,
-      amount.valueAsNumber
-    );
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   } else {
-    var payment = new Payment(
-      tofrom.value,
-      details.value,
-      amount.valueAsNumber
-    ); 
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
   }
+  list.render(doc, "Invoice", "end");
 });
